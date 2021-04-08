5dc58a795d172760026f4c859ca4bfbc125be9d2
Jakob <Jakob.hyldgaard@gmail.com>
2021-04-08 10:25:57 +0200

    cleanup for codeGen

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,10 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String testString = "minespeak \n func Test(param1 : num) -> num do \n var n : num = 5 \n if true do \n n = 2 \n else do \n n = 3 \n endif \n endfunc \n\n\n\n closespeak";
-        testString = "minespeak \n func Test(param1 : num) -> num do \n var n : num = 5 \n if false do \n var b: num = 1 \n else do \n var a: num = 2 \n endif \n endfunc \n\n\n\n closespeak";
-        testString = "minespeak \n func Test() do \n $tp ~0 ~1 ~0 \n endfunc \n closespeak";
-        //testString = "minespeak \n func Test(param1 : num) -> num do \n var n : bool = true \n \n endfunc \n\n\n\n closespeak";
-        String filePath = new File("").getAbsolutePath();
-//        CharStream charStream = CharStreams.fromFileName(filePath + "\\src\\testString.ms");
-        CharStream charStream = CharStreams.fromString(testString);
+    public static void main(String[] args) {
 
-        //CharStream charStream = CharStreams.fromString(testString);
+        String testString = "minespeak \n func Test() do \n $tp ~0 ~1 ~0 \n endfunc \n closespeak";
+        CharStream charStream = CharStreams.fromString(testString);
 
         Logger.shared.setSourceProg(charStream.toString().split(System.getProperty("line.separator")));
 
@@ -29,8 +23,6 @@ public class Main {
         MinespeakParser minespeakParser = new MinespeakParser(commonTokenStream);
         ParseErrorListener parseErrorListener = new ParseErrorListener();
         minespeakParser.addErrorListener(parseErrorListener);
-
-
 
         ParseTree tree = minespeakParser.prog();
         System.out.println(tree.toStringTree(minespeakParser));
@@ -47,37 +39,10 @@ public class Main {
         UnassignedVariableListener unassignedVaribleListener = new UnassignedVariableListener();
         ParseTreeWalker.DEFAULT.walk(unassignedVaribleListener, tree);
 
-        for (FuncEntry entry : walker.functionSignatures.values()) {
-            System.out.print(entry.getName() + ": ");
-            for (SymEntry param : entry.getParams()) {
-                System.out.print(param.getType().toString() + ", ");
-            }
-            System.out.println("\n");
-        }
-
-        //ParseTreeWalker.DEFAULT.walk(scopeListener, tree);
-
         Logger.shared.print();
-
-        //System.out.println(commonTokenStream.get(25, 29));
-//        System.out.println(tree);
-//
-//        System.out.println(Type.resultTypes);
 
         CodeGenVisitor codeGenVisitor = new CodeGenVisitor();
         codeGenVisitor.visit(tree);
-
-        /*Scope scope = new Scope();
-        ScopeVisitorDepre visitor = new ScopeVisitorDepre(scope);
-        visitor.visit(tree);
-
-
-        System.out.println("n is: " + (visitor.getScope().lookup("n") != null ? visitor.getScope().lookup("n").getValue() : "null"));
-        System.out.println("a is: " + (visitor.getScope().lookup("a") != null ? visitor.getScope().lookup("a").getValue() : "null"));
-        System.out.println("b is: " + (visitor.getScope().lookup("b") != null ? visitor.getScope().lookup("b").getValue() : "null"));
-        
-        System.out.println("n is: " + visitor.getScope().lookup("n").getValue());
-*/
     }
 
 }
