9dd650dcb75e05c2d07c2c83fc0255aa0fc0053c
Jakob <Jakob.hyldgaard@gmail.com>
2021-04-14 20:36:01 +0200

    updated visitor return type from Object to Value

--- src/Code-generation/CodeGenVisitor.java
+++ src/Code-generation/CodeGenVisitor.java
@@ -6,7 +6,7 @@ import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CodeGenVisitor extends MinespeakBaseVisitor{
+public class CodeGenVisitor extends MinespeakBaseVisitor<Value>{
     private Scope currentScope;
     private Map<String, FuncEntry> funcSignature;
     private MSValueFactory msValueFactory = new MSValueFactory();
@@ -17,58 +17,97 @@ public class CodeGenVisitor extends MinespeakBaseVisitor{
     }
 
     @Override
-    public Object visitNotNegFac(MinespeakParser.NotNegFacContext ctx) {
+    public Value visitNotNegFac(MinespeakParser.NotNegFacContext ctx) {
 
-        var factorObj = visit(ctx.factor());
+        boolean factorBool= false;
+        int factorNum= 0;
+        Vector2 factorVec2 = null;
+        Vector3 factorVec3 = null;
+        var a = visit(ctx.factor());
 
-        if (ctx.factor().type == Type._bool) {
-            boolean factor = (boolean) factorObj;
-            return ctx.NOT() != null ? !factor : factor;
-        }
-        else if (ctx.factor().type == Type._num){
-            int factor = (int) factorObj;
-            return ctx.SUB() != null ? -factor : factor;
-        }
+        if (ctx.type == Type._bool)
+            factorBool = Value.value(visit(ctx.factor()).getCasted(BoolValue.class));
+        else if (ctx.type == Type._num)
+            factorNum = Value.value(visit(ctx.factor()).getCasted(NumValue.class));
+        else if (ctx.type == Type._vector2)
+            factorVec2 = Value.value(visit(ctx.factor()).getCasted(Vector2Value.class));
+        else if (ctx.type == Type._vector3)
+            factorVec3 = Value.value(visit(ctx.factor()).getCasted(Vector3Value.class));
 
-         return factorObj;
-    }
+        if (ctx.factor().type == Type._bool)
+            return msValueFactory.createValue(ctx.NOT() != null ? !factorBool : factorBool, ctx.type);
+        else if (ctx.factor().type == Type._num)
+            return msValueFactory.createValue(ctx.SUB() != null ? -factorNum : factorNum, ctx.type);
+        else if (ctx.factor().type == Type._vector2)
+            return msValueFactory.createValue(Vector2.neg(factorVec2), Type._vector2);
+        else if (ctx.factor().type == Type._vector3)
+            return msValueFactory.createValue(Vector3.neg(factorVec3), Type._vector3);
 
-    @Override
-    public Object visitPow(MinespeakParser.PowContext ctx) {
-        return Math.pow((double) visit(ctx.expr(0)), (double) visit(ctx.expr(1)));
-    }
-
-    @Override
-    public Object visitMulDivMod(MinespeakParser.MulDivModContext ctx) {
-        return super.visitMulDivMod(ctx);
-    }
-
-    @Override
-    public Object visitAddSub(MinespeakParser.AddSubContext ctx) {
-        int num1 = (int) visit(ctx.expr(0));
-        int num2 = (int) visit(ctx.expr(1));
-
-        if (ctx.ADD() != null)
-            return num1 + num2;
-        else if (ctx.SUB() != null)
-            return num1 - num2;
-        else
-            Error();
-
+        Error();
         return null;
     }
 
     @Override
-    public Object visitNumberLiteral(MinespeakParser.NumberLiteralContext ctx) {
-        int radix = ctx.DecimalDigit() != null ? 10 : 16;
-        String numText = radix == 10 ? ctx.getText(): ctx.getText().substring(2); // cut '0x' from hex
-        return Integer.parseInt(numText, radix);
+    public Value visitPow(MinespeakParser.PowContext ctx) {
+        int num1 = Value.value(visit(ctx.expr(0)).getCasted(NumValue.class));
+        int num2 = Value.value(visit(ctx.expr(1)).getCasted(NumValue.class));
+        return msValueFactory.createValue((int) Math.pow(num1, num2), ctx.type);
     }
 
     @Override
-    public Object visitLiteral(MinespeakParser.LiteralContext ctx) {
+    public Value visitMulDivMod(MinespeakParser.MulDivModContext ctx) {
+        return super.visitMulDivMod(ctx);
+    }
+
+    @Override
+    public Value visitAddSub(MinespeakParser.AddSubContext ctx) {
+        int num1 = 0, num2 = 0;
+        Vector2 vec21 = null, vec22 = null;
+        Vector3 vec31 = null, vec32 = null;
+
+        if (ctx.type == Type._num){
+            num1 = Value.value(visit(ctx.expr(0)).getCasted(NumValue.class));
+            num2 = Value.value(visit(ctx.expr(1)).getCasted(NumValue.class));
+        }
+        else if (ctx.type == Type._vector2){
+            vec21 = Value.value(visit(ctx.expr(0)).getCasted(Vector2Value.class));
+            vec22 = Value.value(visit(ctx.expr(1)).getCasted(Vector2Value.class));
+        }
+        else if (ctx.type == Type._vector3){
+            vec31 = Value.value(visit(ctx.expr(0)).getCasted(Vector3Value.class));
+            vec32 = Value.value(visit(ctx.expr(1)).getCasted(Vector3Value.class));
+        }
+
+        if (ctx.ADD() != null)
+            if (ctx.type == Type._num)
+                return msValueFactory.createValue(num1 + num2, ctx.type);
+            else if (ctx.type == Type._vector2)
+                return msValueFactory.createValue(Vector2.add(vec21, vec22), ctx.type);
+            else if (ctx.type == Type._vector3)
+                return msValueFactory.createValue(Vector3.add(vec31, vec32), ctx.type);
+        else if (ctx.SUB() != null)
+                if (ctx.type == Type._num)
+                    return msValueFactory.createValue(num1 - num2, ctx.type);
+                else if (ctx.type == Type._vector2)
+                    return msValueFactory.createValue(Vector2.sub(vec21, vec22), ctx.type);
+                else if (ctx.type == Type._vector3)
+                    return msValueFactory.createValue(Vector3.sub(vec31, vec32), ctx.type);
+
+        Error();
+        return null;
+    }
+
+    @Override
+    public Value visitNumberLiteral(MinespeakParser.NumberLiteralContext ctx) {
+        int radix = ctx.DecimalDigit() != null ? 10 : 16;
+        String numText = radix == 10 ? ctx.getText(): ctx.getText().substring(2); // cut '0x' from hex
+        return msValueFactory.createValue(Integer.parseInt(numText, radix), Type._num);
+    }
+
+    @Override
+    public Value visitLiteral(MinespeakParser.LiteralContext ctx) {
         if (ctx.type == Type._string || ctx.type == Type._block)
-            return ctx.getText();
+            return msValueFactory.createValue(ctx.getText(), ctx.type);
         else if (ctx.type == Type._num)
             return visit(ctx.numberLiteral());
         else if (ctx.type == Type._bool)
@@ -79,55 +118,62 @@ public class CodeGenVisitor extends MinespeakBaseVisitor{
             return visit(ctx.vector3Literal());
         else if (ctx.rArray() != null)
             return visit(ctx.rArray());
-        else
-            Error();
 
+        Error();
         return null;
     }
 
     @Override
-    public Object visitBooleanLiteral(MinespeakParser.BooleanLiteralContext ctx) {
-        return ctx.TRUE() != null;
+    public Value visitBooleanLiteral(MinespeakParser.BooleanLiteralContext ctx) {
+        return msValueFactory.createValue(ctx.TRUE() != null, Type._bool);
     }
 
     @Override
-    public Object visitVector2Literal(MinespeakParser.Vector2LiteralContext ctx) {
-        return getVector(ctx.vecElement());
+    public Value visitVector2Literal(MinespeakParser.Vector2LiteralContext ctx) {
+        return msValueFactory.createValue(
+                new Vector2(
+                    Value.value(visit(ctx.expr(0)).getCasted(NumValue.class)),
+                    Value.value(visit(ctx.expr(1)).getCasted(NumValue.class))
+                ), Type._vector2);
     }
 
     @Override
-    public Object visitVector3Literal(MinespeakParser.Vector3LiteralContext ctx) {
-        return getVector(ctx.vecElement());
+    public Value visitVector3Literal(MinespeakParser.Vector3LiteralContext ctx) {
+        return msValueFactory.createValue(
+                new Vector3(
+                    Value.value(visit(ctx.expr(0)).getCasted(NumValue.class)),
+                    Value.value(visit(ctx.expr(1)).getCasted(NumValue.class)),
+                    Value.value(visit(ctx.expr(2)).getCasted(NumValue.class))
+                ), Type._vector3);
     }
 
     @Override
-    public Object visitRelations(MinespeakParser.RelationsContext ctx) {
-        var expr1 = visit(ctx.expr(0));
-        var expr2 = visit(ctx.expr(2));
+    public Value visitRelations(MinespeakParser.RelationsContext ctx) {
+        var expr1 = Value.value(visit(ctx.expr(0)).getCasted(NumValue.class));
+        var expr2 = Value.value(visit(ctx.expr(2)).getCasted(NumValue.class));
 
         if (ctx.LESSER() != null)
-            return (int) expr1 < (int) expr2;
+            return msValueFactory.createValue(expr1 < expr2, Type._bool);
         else if (ctx.GREATER() != null)
-            return (int) expr1 > (int) expr2;
+            return msValueFactory.createValue(expr1 > expr2, Type._bool);
         else if (ctx.LESSEQ() != null)
-            return (int) expr1 <= (int) expr2;
+            return msValueFactory.createValue(expr1 <= expr2, Type._bool);
         else if (ctx.GREATEQ() != null)
-            return (int) expr1 >= (int) expr2;
-        else
-            Error();
+            return msValueFactory.createValue(expr1 >= expr2, Type._bool);
 
+        Error();
         return null;
     }
 
     @Override
-    public Object visitEquality(MinespeakParser.EqualityContext ctx) {
+    public Value visitEquality(MinespeakParser.EqualityContext ctx) {
         var expr1 = visit(ctx.expr(0));
         var expr2 = visit(ctx.expr(2));
 
         if (ctx.EQUAL() != null)
-            return expr1 == expr2;
+            return msValueFactory.createValue(expr1 == expr2, Type._bool);
         else if (ctx.NOTEQUAL() != null)
-            return expr1 != expr2;
+            return msValueFactory.createValue(expr1 != expr2, Type._bool);
         else
             Error();
 
@@ -136,17 +182,28 @@ public class CodeGenVisitor extends MinespeakBaseVisitor{
 
 
     @Override
-    public Object visitAnd(MinespeakParser.AndContext ctx) {
-        return (boolean) visit(ctx.expr(0)) && (boolean) visit(ctx.expr(1));
+    public Value visitAnd(MinespeakParser.AndContext ctx) {
+        Value v1 = visit(ctx.expr(0));
+
+        var b1 = Value.value(v1.getCasted(BoolValue.class));
+        Value v2 = visit(ctx.expr(0));
+        var b2 = Value.value(v2.getCasted(BoolValue.class));
+
+        return msValueFactory.createValue((b1 && b2), Type._bool);
     }
 
     @Override
-    public Object visitOr(MinespeakParser.OrContext ctx) {
-        return (boolean) visit(ctx.expr(0)) || (boolean) visit(ctx.expr(1));
+    public Value visitOr(MinespeakParser.OrContext ctx) {
+        Value v1 = visit(ctx.expr(0));
+        var b1 = Value.value(v1.getCasted(BoolValue.class));
+        Value v2 = visit(ctx.expr(0));
+        var b2 = Value.value(v2.getCasted(BoolValue.class));
+
+        return msValueFactory.createValue((b1 || b2), Type._bool);
     }
 
     @Override
-    public Object visitFactor(MinespeakParser.FactorContext ctx) {
+    public Value visitFactor(MinespeakParser.FactorContext ctx) {
         if (ctx.LPAREN() != null)
             return visit(ctx.expr());
         else if (ctx.rvalue() != null)
@@ -154,24 +211,24 @@ public class CodeGenVisitor extends MinespeakBaseVisitor{
         else if (ctx.literal() != null)
             return visit(ctx.literal());
         else if (ctx.funcCall() != null)
-            return ctx.funcCall();
+            return visit(ctx.funcCall());
         else
             return visit(ctx.arrayAccess());
     }
 
     @Override
-    public Object visitRvalue(MinespeakParser.RvalueContext ctx) {
+    public Value visitRvalue(MinespeakParser.RvalueContext ctx) {
         return currentScope.lookup(ctx.ID().getText()).getValue();
     }
 
     @Override
-    public Object visitProg(MinespeakParser.ProgContext ctx) {
+    public Value visitProg(MinespeakParser.ProgContext ctx) {
         currentScope = ctx.scope;
         return super.visitProg(ctx);
     }
 
     @Override
-    public Object visitFunc(MinespeakParser.FuncContext ctx) {
+    public Value visitFunc(MinespeakParser.FuncContext ctx) {
         currentScope = ctx.scope;
         var id = ctx.funcSignature().ID().getText();
         var func = funcSignature.getOrDefault(id, null);
@@ -182,13 +239,13 @@ public class CodeGenVisitor extends MinespeakBaseVisitor{
     }
 
     @Override
-    public Object visitFuncBody(MinespeakParser.FuncBodyContext ctx) {
+    public Value visitFuncBody(MinespeakParser.FuncBodyContext ctx) {
         currentScope = ctx.scope;
         return super.visitFuncBody(ctx);
     }
 
     @Override
-    public Object visitStmnts(MinespeakParser.StmntsContext ctx) {
+    public Value visitStmnts(MinespeakParser.StmntsContext ctx) {
 
         String stmntsText = "";
 
@@ -197,11 +254,11 @@ public class CodeGenVisitor extends MinespeakBaseVisitor{
             stmntsText += (b != null ? b : "");
         }
 
-        return stmntsText;
+        return msValueFactory.createValue(stmntsText, Type._string);
     }
 
     @Override
-    public Object visitStmnt(MinespeakParser.StmntContext ctx) {
+    public Value visitStmnt(MinespeakParser.StmntContext ctx) {
         String command = "";
         if (ctx.MCStmnt() != null) {
             String stmnt = ctx.MCStmnt().getText();
@@ -211,46 +268,74 @@ public class CodeGenVisitor extends MinespeakBaseVisitor{
 
             while (matcher.find()) {
                 String varName = matcher.group("varName");
-                SymEntry varVal = currentScope.lookup(varName);
+                Type type = currentScope.lookup(varName).getType();
+                Value varVal = currentScope.lookup(varName).getValue();
 
+                String formatString ="";
 
-                if (varVal.getType() == Type._vector2 || varVal.getType() == Type._vector3){
-                    String formatString = "";
-                    for (int num: (Vector<Integer>) varVal.getValue()) {
-                        formatString += (PosRelativeToPlayer ? "~" : "^") + num + " ";
-                    }
-                    stmnt = stmnt.replace("v{" + varName + "}", formatString);
-                }
+                if (type == Type._num)
+                    formatString = Value.value(varVal.getCasted(NumValue.class)).toString();
+                else if (type == Type._block)
+                    formatString = Value.value(varVal.getCasted(BlockValue.class));
+                else if (type == Type._bool)
+                    formatString = Value.value(varVal.getCasted(BoolValue.class)).toString();
+                else if (type == Type._string)
+                    formatString = Value.value(varVal.getCasted(StringValue.class));
+                else if (type == Type._vector2)
+                    formatString = Value.value(varVal.getCasted(Vector2Value.class)).toString(PosRelativeToPlayer ? "~" : "^");
+                else if (type == Type._vector3)
+                    formatString = Value.value(varVal.getCasted(Vector3Value.class)).toString(PosRelativeToPlayer ? "~" : "^");
                 else
-                    stmnt = stmnt.replace("v{" + varName + "}", varVal != null ? varVal.getValue().toString() : "MAKELOOKUP");
+                    Error();
+
+                stmnt = stmnt.replace("v{" + varName + "}", formatString);
             }
 
             command = stmnt.replace("$", "");
-            super.visitStmnt(ctx);
-            return new MCStatementST(command).output;
+//            super.visitStmnt(ctx);
+            return msValueFactory.createValue(new MCStatementST(command).output, Type._string);
         }else {
             return super.visitStmnt(ctx);
         }
     }
 
     @Override
-    public Object visitInstan(MinespeakParser.InstanContext ctx) {
+    public Value visitInstan(MinespeakParser.InstanContext ctx) {
         for (int i = 0; i < ctx.ID().size(); i++) {
             String ID = ctx.ID(i).getText();
 
 
-            String exprEval = visit(ctx.initialValue(i).expr()).toString();
-            currentScope.lookup(ID).setValue(exprEval);
+            var expr = ctx.initialValue(i).expr();
+            Value exprEval = visit(expr);
+
+            if (expr.type == Type._num)
+                currentScope.lookup(ID).setValue(Value.value(exprEval.getCasted(NumValue.class)));
+            else if (expr.type == Type._block)
+                currentScope.lookup(ID).setValue(Value.value(exprEval.getCasted(BlockValue.class)));
+            else if (expr.type == Type._bool)
+                currentScope.lookup(ID).setValue(Value.value(exprEval.getCasted(BoolValue.class)));
+            else if (expr.type == Type._string)
+                currentScope.lookup(ID).setValue(Value.value(exprEval.getCasted(StringValue.class)));
+            else if (expr.type == Type._vector2)
+                currentScope.lookup(ID).setValue(Value.value(exprEval.getCasted(Vector2Value.class)));
+            else if (expr.type == Type._vector3)
+                currentScope.lookup(ID).setValue(Value.value(exprEval.getCasted(Vector3Value.class)));
+
         }
 
         return null;
     }
 
     @Override
-    public Object visitFuncCall(MinespeakParser.FuncCallContext ctx) {
+    public Value visitFuncCall(MinespeakParser.FuncCallContext ctx) {
         var expr = ctx.expr(0).getText();
         var func = funcSignature.getOrDefault(ctx.ID().getText(), null);
 
+//        for (int i = 0; i < func.getParams().size(); i++) {
+//            var val = visit(ctx.expr(i));
+//            currentScope.lookup(func.getParams().get(i).getName()).setValue(2);
+//        }
+//        ctx.expr()
         if (func == null)
             return null;
 
@@ -264,16 +349,7 @@ public class CodeGenVisitor extends MinespeakBaseVisitor{
      Helper
      */
 
-    private Vector<Integer> getVector(List<MinespeakParser.VecElementContext> elements) {
-        Vector<Integer> vector = new Vector<>();
-
-        for (var element: elements) {
-            vector.add((int)visit(element));
-        }
-        return vector;
-    }
-
     private void Error(){
-        System.out.println("SHOULD NOT HAPPEND!");
+        System.out.println("SHOULD NOT HAPPEN!");
     }
 }
