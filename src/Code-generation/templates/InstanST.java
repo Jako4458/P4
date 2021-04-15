b5ecb45abb96aff49420289fe03ec571e1e72915
Jakob <Jakob.hyldgaard@gmail.com>
2021-04-15 09:07:06 +0200

    a

--- src/Code-generation/templates/InstanST.java
+++ src/Code-generation/templates/InstanST.java
@@ -3,11 +3,11 @@ import org.stringtemplate.v4.ST;
 public class InstanST {
     public String output;
 
-    public InstanST(Value val){
+    public InstanST(String varName, String value){
         ST st = new ST("scoreboard objectives add <varName> dummy \nscoreboard <varName> add <value> //<Comment>");
         st.add("Comment", this.getClass().toString().substring(16));
 
-        st.add("varName", val.);
+        st.add("varName", varName);
         st.add("value", value);
 
         output = st.render();
