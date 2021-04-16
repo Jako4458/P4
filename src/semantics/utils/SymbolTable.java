38149cca578492eb2b719dd6d6db24ea4a7c2650
ChBLA <cbla19@student.aau.dk>
2021-04-16 11:50:57 +0200

    Removed unused files

--- src/semantics/utils/SymbolTable.java
+++
@@ -1,8 +0,0 @@
-public interface SymbolTable {
-
-    public SymEntry getEntry(String hashCode);
-    public SymEntry addEntry(SymEntry entry);
-    public SymEntry lookup(SymEntry entry);
-    public SymEntry lookup(String hashCode);
-
-}
--- src/semantics/utils/TreeNode.java
+++
@@ -1,4 +0,0 @@
-public interface TreeNode {
-    Node getParent();
-
-}
