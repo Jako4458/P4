21bdd1473c2471359e12ad266ada38b25ff4f4af
ChBLA <cbla19@student.aau.dk>
2021-04-16 09:03:01 +0200

    Templates

--- src/Code-generation/templates/expressions/ArithmeticExprST.java
+++ src/Code-generation/templates/expressions/ArithmeticExprST.java
@@ -32,26 +32,34 @@ public class ArithmeticExprST implements Template {
     private ST createPowTemplate(String a, int b, String prefix, String exprID) {
         ST template;
         if (b == 0) {
-            template = new ST("scoreboard objectives add <exprID> dummy\nscoreboard players set @s <exprID> 1");
+            template = new ST("<prefix> scoreboard objectives add <exprID> dummy\n<prefix> scoreboard players set @s <exprID> 1");
             template.add("exprID", exprID);
-        } else if (b > 0) {
-            template = new ST("scoreboard objectives add <exprID> dummy\nscoreboard players operation @s <exprID> = @s <aID>\n<follow>");
-            template.add("exprID", exprID);
-            template.add("aID", a);
-            String follow = "";
-            for (int i = b - 1; i > 0; i--) {
-                follow = follow.concat(String.format("scoreboard players operation @s %s *= @s %s\n", exprID, a));
-            }
-            template.add("follow", follow);
         } else {
-            template = new ST("scoreboard objectives add <exprID> dummy\nscoreboard players set @s <exprID> 1\n<follow>");
-            template.add("exprID", exprID);
             String follow = "";
-            for (int i = b; i < 0; i++) {
-                follow = follow.concat(String.format("scoreboard players operation @s %s /= @s %s\n", exprID, a));
+            if (b > 0) {
+                template = new ST(
+                        "<prefix> scoreboard objectives add <exprID> dummy\n" +
+                                "<prefix> scoreboard players operation @s <exprID> = @s <aID>\n" +
+                                "<follow>");
+
+                template.add("aID", a);
+                for (int i = b - 1; i > 0; i--) {
+                    follow = follow.concat(String.format("%s scoreboard players operation @s %s *= @s %s\n", prefix, exprID, a));
+                }
+            } else {
+                template = new ST(
+                        "<prefix> scoreboard objectives add <exprID> dummy\n" +
+                                "<prefix> scoreboard players set @s <exprID> 1\n" +
+                                "<follow>");
+                for (int i = b; i < 0; i++) {
+                    follow = follow.concat(String.format("%s scoreboard players operation @s %s /= @s %s\n", prefix, exprID, a));
+                }
+
             }
+            template.add("exprID", exprID);
             template.add("follow", follow);
         }
+        template.add("prefix", prefix);
         return template;
     }
 
--- src/Code-generation/templates/expressions/EqualityExprST.java
+++ src/Code-generation/templates/expressions/EqualityExprST.java
@@ -1,3 +1,24 @@
+import org.stringtemplate.v4.ST;
 
-public class EqualityExprST {
+public class EqualityExprST implements Template {
+    private String output;
+
+    public EqualityExprST(String a, String b, String operator, String prefix, String exprID) {
+        ST template = new ST(
+                "<prefix> scoreboard objectives add <exprID> dummy\n" +
+                        "<prefix> scoreboard players set @s <exprID> 0\n" +
+                        "<prefix> execute <condition> score @s <aID> = @s <bID> run scoreboard players set @s <exprID> 1"
+        );
+        template.add("prefix", prefix);
+        template.add("aID", a);
+        template.add("bID", b);
+        template.add("exprID", exprID);
+        template.add("condition", operator.equals("==") ? "if" : "unless");
+        this.output = template.render();
+    }
+
+    @Override
+    public String getOutput() {
+        return this.output;
+    }
 }
