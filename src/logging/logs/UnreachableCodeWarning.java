3ec68b25c4e5b46167e65a61af79eebd929fc70d
StevenDankMeister <stran19@student.aau.dk>
2021-04-26 11:13:55 +0200

    unreachable code log

--- src/logging/logs/UnreachableCodeWarning.java
+++ src/logging/logs/UnreachableCodeWarning.java
@@ -1,2 +1,13 @@
-package logging.logs;public class UnreachableCodeWarning {
+package logging.logs;
+
+public class UnreachableCodeWarning extends WarningLog {
+
+    public UnreachableCodeWarning(String text, int lineNum, int characterIndex) {
+        super(text, lineNum, characterIndex);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: expression %s leads to unreachable body.", super.toString(), getText());
+    }
 }
