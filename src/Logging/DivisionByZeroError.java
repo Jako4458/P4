81268e7781d545e17031310145b984366493fd83
ChBLA <cbla19@student.aau.dk>
2021-04-26 15:54:30 +0200

    Division by zero check

---
+++ src/Logging/DivisionByZeroError.java
@@ -0,0 +1,12 @@
+package Logging;
+
+public class DivisionByZeroError extends ErrorLog {
+    public DivisionByZeroError(String text, int lineNum, int characterIndex) {
+        super(text, lineNum, characterIndex);
+    }
+
+    @Override
+    public String toString() {
+        return "CompileError: Cannot divide by 0";
+    }
+}
