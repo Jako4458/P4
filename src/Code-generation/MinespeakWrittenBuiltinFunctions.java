3dada64238e02ddf447609de2476aabddc4c6fbc
StevenDankMeister <stran19@student.aau.dk>
2021-05-18 13:43:11 +0200

    implemented dig

--- src/Code-generation/MinespeakWrittenBuiltinFunctions.java
+++ src/Code-generation/MinespeakWrittenBuiltinFunctions.java
@@ -8,22 +8,33 @@ public class MinespeakWrittenBuiltinFunctions {
     public MinespeakWrittenBuiltinFunctions() {
         functions = new ArrayList<>();
         functions.add(minespeakFill(System.lineSeparator()));
+        functions.add(minespeakDig(System.lineSeparator()));
     }
 
-    public ST minespeakFill(String lineSeperator) {
-        ST st = new ST("func fill(from:vector3, x:num, y:num, z:num, b:block, relative:bool) do<lineSeperator>" +
-                               "if not(x<OP>0 or y<OP>0 or z<OP>0) do<lineSeperator>" +
-                               "for var i:num=0 until i==x where i+=1 do<lineSeperator>" +
-                               "for var j:num=0 until j==y where j+=1 do<lineSeperator>" +
-                               "for var k:num=0 until k==z where k+=1 do<lineSeperator>" +
-                               "setB(from + <TO>, b, relative)<lineSeperator>" +
-                               "endfor<lineSeperator>endfor<lineSeperator>endfor<lineSeperator>" +
+    public ST minespeakFill(String lineSeparator) {
+        ST st = new ST("func fill(from:vector3, x:num, y:num, z:num, b:block, relative:bool) do<lineSeparator>" +
+                               "if not(x<OP>0 or y<OP>0 or z<OP>0) do<lineSeparator>" +
+                               "for var i:num=0 until i==x where i+=1 do<lineSeparator>" +
+                               "for var j:num=0 until j==y where j+=1 do<lineSeparator>" +
+                               "for var k:num=0 until k==z where k+=1 do<lineSeparator>" +
+                               "setB(from + <TO>, b, relative)<lineSeparator>" +
+                               "endfor<lineSeparator>endfor<lineSeparator>endfor<lineSeparator>" +
                                "endif" +
-                               "<lineSeperator>endfunc<lineSeperator>");
+                               "<lineSeparator>endfunc<lineSeparator>");
 
-        st.add("lineSeperator", lineSeperator);
+        st.add("lineSeparator", lineSeparator);
         st.add("OP", "<=");
         st.add("TO", "<i,j,k>");
+
+        return st;
+    }
+
+    public ST minespeakDig(String lineSeparator) {
+        ST st = new ST("func dig(from:vector3, x:num, y:num, z:num, relative:bool) do<lineSeparator>" +
+                "fill(from, x, y, z, #air, relative)" +
+                "<lineSeparator>endfunc<lineSeparator>");
+
+        st.add("lineSeparator", lineSeparator);
 
         return st;
     }
