c111679eb9b3aa4bd498594400fd883830f3dd99
ChBLA <cbla19@student.aau.dk>
2021-05-26 19:47:17 +0200

    Reorder

--- src/semantics/utils/SymEntry.java
+++ src/semantics/utils/SymEntry.java
@@ -2,12 +2,10 @@ import org.antlr.v4.runtime.ParserRuleContext;
 
 public interface SymEntry {
     String getName();
+    Type getType();
+    ParserRuleContext getCtx();
+    int getModifier();
+
     void setName(String newName);
     String getVarName(boolean readableNames);
-
-    Type getType();
-
-    ParserRuleContext getCtx();
-
-    int getModifier();
 }
